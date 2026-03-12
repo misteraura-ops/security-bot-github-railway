981f58cef51e0d15ee3a7722ccc71178c261c981
misteraura-ops <seekkababxp@gmail.com>
2026-03-12 23:55:41 +0300

    hello

--- commands/fun/fuck.js
+++ commands/fun/fuck.js
@@ -1,42 +1,44 @@
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
-    name: 'ss',
+    // ----------------------
+    // CHANGE THIS TO RENAME THE COMMAND
+    // ----------------------
+    name: 'fuck', // <-- You can change this to anything like 'screenshot' or 'image'
     description: 'Display a screenshot or image in an embed.',
+
     async execute(message, args) {
 
         // ----------------------
-        // Resolve target user
+        // Get the mentioned user
         // ----------------------
         const target = message.mentions.users.first();
-        if (!target) {
-            return message.channel.send('❌ Please mention a user!');
-        }
+        if (!target) return message.channel.send('❌ Please mention a user!');
 
         // ----------------------
-        // Dynamic display text
+        // DISPLAY TEXT (editable)
         // ----------------------
-        const displayText = ` **${message.author} fucked ${target}** for the **1st time**`; 
-        // <-- Highlight this for text; @user1 = message.author, @user2 = target
+        const displayText = `💬 **${message.author} fucked ${target}** for the **1st time**`;
+        // <-- Edit this line to change the text format
 
         // ----------------------
-        // INSERT IMAGE LINK HERE
+        // IMAGE LINK (editable)
         // ----------------------
-        const imageLink = 'https://media.discordapp.net/attachments/1480184670606983240/1481755017307357395/fuck10.gif?ex=69b47765&is=69b325e5&hm=a7383b9f7eeeba43c8d74ffa1782a6a3994b6cab34bb584ba62f5851b8ccc7bf&='; 
-        // <-- Highlight this for image
+        const imageLink = 'https://media.discordapp.net/attachments/1480184670606983240/1481755017307357395/fuck10.gif?ex=69b47765&is=69b325e5&hm=a7383b9f7eeeba43c8d74ffa1782a6a3994b6cab34bb584ba62f5851b8ccc7bf&=';
+        // <-- Edit this line to change the image URL
 
         // ----------------------
-        // Create embed
+        // Create the embed
         // ----------------------
         const embed = new EmbedBuilder()
             .setTitle(`Screenshot / Image for ${target.tag}`)
             .setDescription(displayText)
             .setColor('#3498db')
             .setTimestamp()
-            .setImage(imageLink) // Big image at the bottom
+            .setImage(imageLink); // Big image at the bottom
 
         // ----------------------
-        // Send embed
+        // Send the embed
         // ----------------------
         message.channel.send({ embeds: [embed] });
     }
